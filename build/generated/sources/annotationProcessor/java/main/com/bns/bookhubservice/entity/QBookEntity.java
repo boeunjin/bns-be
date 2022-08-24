c7194aa46b7a1092e9659cf770aff185e2665419
이대영/클라우드CoE <101620963+2Daeyoung@users.noreply.github.com>
2022-08-24 13:29:56 +0900

    Add files via upload

---
+++ build/generated/sources/annotationProcessor/java/main/com/bns/bookhubservice/entity/QBookEntity.java
@@ -0,0 +1,53 @@
+package com.bns.bookhubservice.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QBookEntity is a Querydsl query type for BookEntity
+ */
+@Generated("com.querydsl.codegen.EntitySerializer")
+public class QBookEntity extends EntityPathBase<BookEntity> {
+
+    private static final long serialVersionUID = -1689129978L;
+
+    public static final QBookEntity bookEntity = new QBookEntity("bookEntity");
+
+    public final StringPath author = createString("author");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final BooleanPath isRented = createBoolean("isRented");
+
+    public final NumberPath<Integer> ownerId = createNumber("ownerId", Integer.class);
+
+    public final StringPath publisher = createString("publisher");
+
+    public final DatePath<java.time.LocalDate> regDate = createDate("regDate", java.time.LocalDate.class);
+
+    public final NumberPath<Integer> rentCount = createNumber("rentCount", Integer.class);
+
+    public final StringPath thumbnailUrl = createString("thumbnailUrl");
+
+    public final StringPath title = createString("title");
+
+    public QBookEntity(String variable) {
+        super(BookEntity.class, forVariable(variable));
+    }
+
+    public QBookEntity(Path<? extends BookEntity> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QBookEntity(PathMetadata metadata) {
+        super(BookEntity.class, metadata);
+    }
+
+}
+
---
+++ build/generated/sources/annotationProcessor/java/main/com/bns/bookhubservice/entity/QMemberEntity.java
@@ -0,0 +1,49 @@
+package com.bns.bookhubservice.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QMemberEntity is a Querydsl query type for MemberEntity
+ */
+@Generated("com.querydsl.codegen.EntitySerializer")
+public class QMemberEntity extends EntityPathBase<MemberEntity> {
+
+    private static final long serialVersionUID = -217029513L;
+
+    public static final QMemberEntity memberEntity = new QMemberEntity("memberEntity");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath memberEmail = createString("memberEmail");
+
+    public final StringPath memberId = createString("memberId");
+
+    public final StringPath memberLocation = createString("memberLocation");
+
+    public final StringPath memberName = createString("memberName");
+
+    public final StringPath memberSlackId = createString("memberSlackId");
+
+    public final StringPath memberTeamName = createString("memberTeamName");
+
+    public QMemberEntity(String variable) {
+        super(MemberEntity.class, forVariable(variable));
+    }
+
+    public QMemberEntity(Path<? extends MemberEntity> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QMemberEntity(PathMetadata metadata) {
+        super(MemberEntity.class, metadata);
+    }
+
+}
+
